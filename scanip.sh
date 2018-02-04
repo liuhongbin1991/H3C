3abb51c766561f474450f9a23fd90dbde0394646
liuhongbin1991 <374100289@qq.com>
2018-02-04 14:04:06 +0800

    create
    support for scan ips which can be used in the network. Begin with the ip you input.

---
+++ scanip.sh
@@ -0,0 +1,23 @@
+#/bin/bash
+#this shell is used for scan all used ip and echo the ips who are useable
+#Time 2017-02-13
+#Auth liu.hongbin@h3c.com
+
+#define a subnet
+read -p "please input a subnet like 192.168.1.0/24:" orgnetwork
+if test -z $orgnetwork
+then
+	echo "error input";
+	exit
+else
+	network=`echo $orgnetwork |awk -F'.' 'BEGIN{OFS="."}{print $1,$2,$3}'`;
+	for ((i=`echo $orgnetwork |awk -F '/' '{print $1}' |awk -F'.' 'BEGIN{OFS="."}{print $4}'`;i<=254;i++));
+	do 
+		if ping -c 1 -w 0.1 $network\.$i |grep "100% packet loss" >/dev/null 2>&1;
+		then
+		  	#echo "haha" >/dev/null 2>&1;
+			echo $network\.$i can be used;
+		fi 
+		 
+	done;
+fi
